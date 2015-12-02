1c6ae4675662b8cb353d09b4260c8c856c54627e
Amir <baruch.amir@gmail.com>
2015-12-03 00:24:14 +0100

    buttons start and end in place start starts an interval in case not running end clears interval

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
 
 // singletone
+// var state;
 var timer = {
+  intervalid:0,
+  status: 0, // is it runnning
   startTime: 25*60, //25 minutes
   start: function() {
-      // alert(this.startTime);
+      if (this.status === 0) {
+        this.intervalid = setInterval(function(){
+            --timer.startTime;
+            $(".timer").text(timer.startTime);
+          },1000);
+          this.status = 1;
+          console.log("started");
+      } else{
+          console.log("already running");
+      }
 
+
+
+  },
+  end: function() {
+      clearInterval(this.intervalid);
+      console.log("stopped");
+      this.status = 0;
   }
 };
 
+
+
 $(document).ready(function($) {
   $(".timer").text(timer.startTime);
-  setInterval(function(){
-      --timer.startTime;
-      $(".timer").text(timer.startTime);
-    },1000);
-  timer.start();
+
+
+
+  $('#bStart').on('click', function (e) {
+    timer.start();
+  });
+  $('#bEnd').on('click', function (e) {
+    timer.end();
+  });
 });
