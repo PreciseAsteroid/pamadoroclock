6184219200521b8ba165ef89bde214f9f56661a4
Amir <baruch.amir@gmail.com>
2015-12-04 23:17:41 +0100

    added button to increase and decrease session time

--- js/main.js
+++ js/main.js
@@ -24,6 +24,14 @@ var timer = {
       clearInterval(this.intervalid);
       console.log("stopped");
       this.status = 0;
+  },
+  updateStartTime: function(updateVal) {
+      this.end();
+      console.log(timer.startTime);
+      this.startTime = this.startTime + updateVal;
+      console.log("raised start time by " + updateVal);
+      $(".timer").text(timer.startTime);
+
   }
 };
 
@@ -32,12 +40,28 @@ var timer = {
 $(document).ready(function($) {
   $(".timer").text(timer.startTime);
 
-
-
   $('#bStart').on('click', function (e) {
     timer.start();
   });
   $('#bEnd').on('click', function (e) {
     timer.end();
   });
+  $('#bPlus').on('click', function (e) {
+    timer.updateStartTime(60);
+  });
+  $('#bMinus').on('click', function (e) {
+    timer.updateStartTime(-60);
+  });
+//   var data = [4, 8, 15, 16, 23, 42];
+//
+// var x = d3.scale.linear()
+//     .domain([0, d3.max(data)])
+//     .range([0, 420]);
+//
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data([15])
+//   .enter().append("div")
+//     .style("width", function(d) { return x(d) + "px"; })
+//     .text(function(d) { return d; });
 });
